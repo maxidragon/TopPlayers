1efec06e77f19e7c042d7697dfbe14d5f6e992aa
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-03-28 18:50:20 +0200

    Frontend works, start work on caching

---
+++ frontend/src/components/PlayersTable.tsx
@@ -0,0 +1,36 @@
+import {Link, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
+
+
+
+const PlayersTable = (props: any) => {
+    return (
+        <div className="ranking-table-container">
+            <Table>
+                <TableHead className="ranking-table-head">
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Competition</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.players.map((player: any) => (
+                        <TableRow key={player.id}>
+                            <TableCell>
+                                <Link href={player.profile} underline="none" target="_blank">
+                                    {player.name}
+                                </Link>
+                            </TableCell>
+                            <TableCell>
+                                <Link href={player.compWebsite} underline="none" target="_blank">
+                                    {player.competition}
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
+
+export default PlayersTable;
--- frontend/src/components/TopPlayers.tsx
+++ frontend/src/components/TopPlayers.tsx
@@ -1,21 +1,61 @@
-import {Icon, IconButton, LinearProgress, Paper, Tooltip, Typography} from "@mui/material";
+import {Box, CircularProgress, Paper, Typography} from "@mui/material";
 import React from "react";
 import EventSelect from "./EventSelect";
+import PlayersTable from "./PlayersTable";
+import events from "../events";
 
-export default class TopPlayers extends React.Component {
+interface State {
+    isLoading: boolean;
+    players: any[];
+    event: {
+        id: string;
+        name: string;
+        icon: string;
+    };
+}
+//TODO
+//Add error
+export default class TopPlayers extends React.Component<{}, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            players: [],
+            event: events[0],
+            isLoading: true
+        }
+        this.handleEventChange = this.handleEventChange.bind(this);
+    }
+    componentDidMount() {
+        this.getTopPlayers();
+    }
+
+    async getTopPlayers() {
+        this.setState({isLoading: true});
+        console.log("in progress");
+        const response = await fetch(`http://localhost:5000/players/this/${this.state.event.id}`);
+        const data = await response.json();
+        this.setState({players: data, isLoading: false});
+    }
+    async handleEventChange(selectedEvent: any) {
+        console.log(selectedEvent);
+        this.setState({event: selectedEvent});
+        await this.getTopPlayers();
+    }
     render() {
         return (
- <div style={{ textAlign: 'center' }}>
-        <Typography variant="h5">
-         Top players for this weekend
-        </Typography>
-        <EventSelect />
-        <div>
-          <Paper>
-           Top players will be displayed here
-          </Paper>
-        </div>
-      </div>
+            <div style={{textAlign: 'center'}}>
+                <Typography variant="h5">
+                    Top players for this weekend
+                </Typography>
+                <EventSelect selectedEvent={this.state.event} eventChange={this.handleEventChange}/>
+                <div>
+                    <Paper>
+                        {this.state.isLoading ? <div><Box sx={{textAlign: 'center'}}>
+                            <CircularProgress/>
+                        </Box></div> : <PlayersTable players={this.state.players}/>}
+                    </Paper>
+                </div>
+            </div>
         );
     }
 }
