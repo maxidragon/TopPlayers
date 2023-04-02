595de79c945e4a1c155b5a0ade513d697650d12b
Maksymilian Gala <mgwspinanie2006@wp.pl>
2023-04-02 19:52:28 +0200

    Add build

--- backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CompetitionsModule } from './competitions/competitions.module';
-import { CompetitorsModule } from './competitors/competitors.module';
-import { PlayersModule } from './players/players.module';
-
-@Module({
-  imports: [CompetitionsModule, CompetitorsModule, PlayersModule],
-})
-export class AppModule {}
